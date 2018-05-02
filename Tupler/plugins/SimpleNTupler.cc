9833918eadc1d99ff6e82e9e1d991ce0459700c0
rijuvenator <jujudg1031@gmail.com>
2018-05-02 13:08:59 +0200

    use range-based for loop for looping over trigger path names; reindented a few things; removed outdated commented code; added an isMC parameter to the python config

--- Tupler/plugins/SimpleNTupler.cc
+++ Tupler/plugins/SimpleNTupler.cc
@@ -46,139 +46,140 @@
 // class declaration
 class SimpleNTupler : public edm::EDAnalyzer
 {
-public:
-  SimpleNTupler(const edm::ParameterSet&);
-  ~SimpleNTupler() {};
+	public:
+		SimpleNTupler(const edm::ParameterSet&);
+		~SimpleNTupler() {};
 
-private:
-  virtual void beginJob() {};
-  virtual void analyze(const edm::Event&, const edm::EventSetup&);
-  virtual void endJob() { tree.Write(); }
+	private:
+		virtual void beginJob() {};
+		virtual void analyze(const edm::Event&, const edm::EventSetup&);
+		virtual void endJob() { tree.Write(); }
 
-  TreeContainer tree;
+		TreeContainer tree;
 
-  EventBranches    eventData;
-  TriggerBranches  triggerData;
-  BeamspotBranches beamspotData;
-  VertexBranches   vertexData;
-  GenBranches      genData;
-  MuonBranches     muonData;
-  DSAMuonBranches  dsaMuonData;
-  RSAMuonBranches  rsaMuonData;
-  DimuonBranches   dimData;
+		EventBranches    eventData;
+		TriggerBranches  triggerData;
+		BeamspotBranches beamspotData;
+		VertexBranches   vertexData;
+		GenBranches      genData;
+		MuonBranches     muonData;
+		DSAMuonBranches  dsaMuonData;
+		RSAMuonBranches  rsaMuonData;
+		DimuonBranches   dimData;
 
-  edm::EDGetTokenT<pat::TriggerEvent          > triggerEventToken;
-  edm::EDGetTokenT<pat::PackedTriggerPrescales> prescalesToken;
-  edm::EDGetTokenT<edm::TriggerResults        > triggerToken;
-  edm::EDGetTokenT<reco::BeamSpot             > beamspotToken;
-  edm::EDGetTokenT<reco::VertexCollection     > vertexToken;
-  edm::EDGetTokenT<reco::GenParticleCollection> genToken;
-  edm::EDGetTokenT<GenEventInfoProduct        > GEIPToken;
-  edm::EDGetTokenT<pat::MuonCollection        > muonToken;
-  edm::EDGetTokenT<reco::TrackCollection      > dsaMuonToken;
-  edm::EDGetTokenT<reco::TrackCollection      > rsaMuonToken;
+		edm::EDGetTokenT<pat::TriggerEvent          > triggerEventToken;
+		edm::EDGetTokenT<pat::PackedTriggerPrescales> prescalesToken;
+		edm::EDGetTokenT<edm::TriggerResults        > triggerToken;
+		edm::EDGetTokenT<reco::BeamSpot             > beamspotToken;
+		edm::EDGetTokenT<reco::VertexCollection     > vertexToken;
+		edm::EDGetTokenT<reco::GenParticleCollection> genToken;
+		edm::EDGetTokenT<GenEventInfoProduct        > GEIPToken;
+		edm::EDGetTokenT<pat::MuonCollection        > muonToken;
+		edm::EDGetTokenT<reco::TrackCollection      > dsaMuonToken;
+		edm::EDGetTokenT<reco::TrackCollection      > rsaMuonToken;
 
-  std::vector<std::string> ddmHLTPaths;
+		std::vector<std::string> ddmHLTPaths;
 };
 
 SimpleNTupler::SimpleNTupler(const edm::ParameterSet& iConfig):
-  tree("DDTree", ""),
-  eventData   (tree),
-  triggerData (tree),
-  beamspotData(tree),
-  vertexData  (tree),
-  genData     (tree),
-  muonData    (tree),
-  dsaMuonData (tree),
-  rsaMuonData (tree),
-  dimData     (tree),
-  triggerEventToken(consumes<pat::TriggerEvent       >(iConfig.getParameter<edm::InputTag>("triggerEvent"  ))),
-  prescalesToken(consumes<pat::PackedTriggerPrescales>(iConfig.getParameter<edm::InputTag>("prescales"     ))),
-  triggerToken (consumes<edm::TriggerResults         >(iConfig.getParameter<edm::InputTag>("triggerResults"))),
-  beamspotToken(consumes<reco::BeamSpot              >(iConfig.getParameter<edm::InputTag>("beamspot"      ))),
-  vertexToken  (consumes<reco::VertexCollection      >(iConfig.getParameter<edm::InputTag>("vertices"      ))),
-  genToken     (consumes<reco::GenParticleCollection >(iConfig.getParameter<edm::InputTag>("gens"          ))),
-  GEIPToken    (consumes<GenEventInfoProduct         >(iConfig.getParameter<edm::InputTag>("GEIP"          ))),
-  muonToken    (consumes<pat::MuonCollection         >(iConfig.getParameter<edm::InputTag>("muons"         ))),
-  dsaMuonToken (consumes<reco::TrackCollection       >(iConfig.getParameter<edm::InputTag>("dsaMuons"      ))),
-  rsaMuonToken (consumes<reco::TrackCollection       >(iConfig.getParameter<edm::InputTag>("rsaMuons"      ))),
+	tree("DDTree", ""),
 
-  ddmHLTPaths(iConfig.getParameter<std::vector<std::string> >("ddmHLTPaths"))
+	eventData   (tree),
+	triggerData (tree),
+	beamspotData(tree),
+	vertexData  (tree),
+	genData     (tree),
+	muonData    (tree),
+	dsaMuonData (tree),
+	rsaMuonData (tree),
+	dimData     (tree),
+
+	triggerEventToken(consumes<pat::TriggerEvent          >(iConfig.getParameter<edm::InputTag>("triggerEvent"  ))),
+	prescalesToken   (consumes<pat::PackedTriggerPrescales>(iConfig.getParameter<edm::InputTag>("prescales"     ))),
+	triggerToken     (consumes<edm::TriggerResults        >(iConfig.getParameter<edm::InputTag>("triggerResults"))),
+	beamspotToken    (consumes<reco::BeamSpot             >(iConfig.getParameter<edm::InputTag>("beamspot"      ))),
+	vertexToken      (consumes<reco::VertexCollection     >(iConfig.getParameter<edm::InputTag>("vertices"      ))),
+	genToken         (consumes<reco::GenParticleCollection>(iConfig.getParameter<edm::InputTag>("gens"          ))),
+	GEIPToken        (consumes<GenEventInfoProduct        >(iConfig.getParameter<edm::InputTag>("GEIP"          ))),
+	muonToken        (consumes<pat::MuonCollection        >(iConfig.getParameter<edm::InputTag>("muons"         ))),
+	dsaMuonToken     (consumes<reco::TrackCollection      >(iConfig.getParameter<edm::InputTag>("dsaMuons"      ))),
+	rsaMuonToken     (consumes<reco::TrackCollection      >(iConfig.getParameter<edm::InputTag>("rsaMuons"      ))),
+
+	ddmHLTPaths(iConfig.getParameter<std::vector<std::string> >("ddmHLTPaths"))
 {};
 
 void SimpleNTupler::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-  // to be improved later
-  // bool isMC = false;
-  bool isMC = true;
+	// to be improved later
+	// bool isMC = false;
+	bool isMC = true;
 
-  eventData.Fill(iEvent);
+	eventData.Fill(iEvent);
 
-  // Trigger: check whether the DDM path(s) fired or not and if yes,
-  // save the Level-1/HLT muons.
-  bool ddm_paths_fired = false;
-  edm::Handle<pat::TriggerEvent> triggerEvent;
-  iEvent.getByToken(triggerEventToken, triggerEvent);
-  if (triggerEvent.failedToGet()) {
-    edm::LogWarning("SimpleNTupler")
-      << "+++ Warning: pat::TriggerEvent collection is not found +++";
-  }
-  else {
-    edm::Handle<edm::TriggerResults> triggerResults;
-    iEvent.getByToken(triggerToken, triggerResults);
-    if (triggerResults.failedToGet()) {
-      edm::LogWarning("SimpleNTupler")
-    	<< "+++ Warning: edm::TriggerResults collection is not found +++";
-    }
-    else {
-      edm::Handle<pat::PackedTriggerPrescales> prescales;
-      iEvent.getByToken(prescalesToken, prescales);
-      const edm::TriggerNames& triggerNames = iEvent.triggerNames(*triggerResults);
-      ddm_paths_fired =
-	triggerData.Fill(*triggerEvent, prescales, triggerNames, ddmHLTPaths);
-    }
-  }
+	// Trigger: check whether the DDM path(s) fired or not and if yes,
+	// save the Level-1/HLT muons.
+	bool ddm_paths_fired = false;
+	edm::Handle<pat::TriggerEvent> triggerEvent;
+	iEvent.getByToken(triggerEventToken, triggerEvent);
+	if (triggerEvent.failedToGet())
+	{
+		edm::LogWarning("SimpleNTupler")
+			<< "+++ Warning: pat::TriggerEvent collection is not found +++";
+	}
+	else
+	{
+		edm::Handle<edm::TriggerResults> triggerResults;
+		iEvent.getByToken(triggerToken, triggerResults);
+		if (triggerResults.failedToGet())
+		{
+			edm::LogWarning("SimpleNTupler")
+				<< "+++ Warning: edm::TriggerResults collection is not found +++";
+		}
+		else
+		{
+			edm::Handle<pat::PackedTriggerPrescales> prescales;
+			iEvent.getByToken(prescalesToken, prescales);
+			const edm::TriggerNames& triggerNames = iEvent.triggerNames(*triggerResults);
+			ddm_paths_fired =
+				triggerData.Fill(*triggerEvent, prescales, triggerNames, ddmHLTPaths);
+		}
+	}
 
-  // Probably we do not want to do the rest if the path(s) we are
-  // interested in have not fired.
-  if (!ddm_paths_fired) return;
+	// Do nothing else if the path(s) we are interested in have not fired
+	if (!ddm_paths_fired) return;
 
-  // edm::Handle<edm::TriggerResults> triggerResults;
-  // iEvent.getByToken(triggerToken, triggerResults);
-  // const edm::TriggerNames& triggerNames = iEvent.triggerNames(*triggerResults);
-  // triggerData.Fill(*triggerResults, triggerNames);
+	edm::Handle<reco::BeamSpot> beamspot;
+	iEvent.getByToken(beamspotToken, beamspot);
+	beamspotData.Fill(*beamspot);
 
-  edm::Handle<reco::BeamSpot> beamspot;
-  iEvent.getByToken(beamspotToken, beamspot);
-  beamspotData.Fill(*beamspot);
+	edm::Handle<reco::VertexCollection> vertices;
+	iEvent.getByToken(vertexToken, vertices);
+	vertexData.Fill(*vertices);
 
-  edm::Handle<reco::VertexCollection> vertices;
-  iEvent.getByToken(vertexToken, vertices);
-  vertexData.Fill(*vertices);
+	if (isMC)
+	{
+		edm::Handle<reco::GenParticleCollection> gens;
+		edm::Handle<GenEventInfoProduct> GEIP;
+		iEvent.getByToken(genToken, gens);
+		iEvent.getByToken(GEIPToken, GEIP);
+		genData.Fill(*gens, *GEIP);
+	}
 
-  if (isMC) {
-    edm::Handle<reco::GenParticleCollection> gens;
-    edm::Handle<GenEventInfoProduct> GEIP;
-    iEvent.getByToken(genToken, gens);
-    iEvent.getByToken(GEIPToken, GEIP);
-    genData.Fill(*gens, *GEIP);
-  }
-  
-  edm::Handle<pat::MuonCollection> muons;
-  iEvent.getByToken(muonToken, muons);
-  muonData.Fill(*muons);
+	edm::Handle<pat::MuonCollection> muons;
+	iEvent.getByToken(muonToken, muons);
+	muonData.Fill(*muons);
 
-  edm::Handle<reco::TrackCollection> dsaMuons;
-  iEvent.getByToken(dsaMuonToken, dsaMuons);
-  dsaMuonData.Fill(*dsaMuons, *vertices);
+	edm::Handle<reco::TrackCollection> dsaMuons;
+	iEvent.getByToken(dsaMuonToken, dsaMuons);
+	dsaMuonData.Fill(*dsaMuons, *vertices);
 
-  edm::Handle<reco::TrackCollection> rsaMuons;
-  iEvent.getByToken(rsaMuonToken, rsaMuons);
-  rsaMuonData.Fill(*rsaMuons, *vertices);
+	edm::Handle<reco::TrackCollection> rsaMuons;
+	iEvent.getByToken(rsaMuonToken, rsaMuons);
+	rsaMuonData.Fill(*rsaMuons, *vertices);
 
-  dimData.Fill(iSetup, *dsaMuons, *vertices);
+	dimData.Fill(iSetup, *dsaMuons, *vertices);
 
-  tree.Fill();
+	tree.Fill();
 };
 
 // make plugin
--- Tupler/python/Modules/SimpleNTupler_cfi.py
+++ Tupler/python/Modules/SimpleNTupler_cfi.py
@@ -5,24 +5,37 @@ TFileService = cms.Service('TFileService', fileName = cms.string('output_ntuple.
 # Assumes that this module is run on the output of PATFilter module.
 SimpleNTupler = cms.EDAnalyzer('SimpleNTupler',
 	cms.untracked.PSet(
-                # Trigger block
-                triggerEvent = cms.InputTag('patTriggerEvent'),
-                prescales = cms.InputTag('patTrigger', 'l1max', 'PAT'),
-                # Versions to be checked for other data sets
-                ddmHLTPaths = cms.vstring("HLT_L2DoubleMu28_NoVertex_2Cha_Angle2p5_Mass10_v4", # 2016D data
-                                          "HLT_L2DoubleMu28_NoVertex_2Cha_Angle2p5_Mass10_v6"  # MC
-                                          ), 
-                # triggerResults = cms.InputTag('TriggerResults', '', 'PAT'), # this contains the results of "good data" filters
+
+####################
+#### PARAMETERS ####
+####################
+
+		isMC           = cms.bool(False),
+
+#######################
+#### TRIGGER BLOCK ####
+#######################
+		triggerEvent   = cms.InputTag('patTriggerEvent'),
+		prescales      = cms.InputTag('patTrigger', 'l1max', 'PAT'),
+
+		# Versions to be checked for other data sets
+		ddmHLTPaths    = cms.vstring(
+			"HLT_L2DoubleMu28_NoVertex_2Cha_Angle2p5_Mass10_v4", # 2016D data
+			"HLT_L2DoubleMu28_NoVertex_2Cha_Angle2p5_Mass10_v6"  # MC
+		),
+#		triggerResults = cms.InputTag('TriggerResults', '', 'PAT'), # this contains the results of "good data" filters
 		triggerResults = cms.InputTag('TriggerResults', '', 'HLT'),
-                #
-		beamspot = cms.InputTag('offlineBeamSpot'),
-		vertices = cms.InputTag('offlinePrimaryVertices'),
-#		muons = cms.InputTag('selectedPatMuons'),
-		muons = cms.InputTag('cleanPatMuons'),
-#		gens = cms.InputTag('genParticles'),
-		gens = cms.InputTag('prunedGenParticles', '', 'PAT'),
-		GEIP = cms.InputTag('generator'),
-		dsaMuons = cms.InputTag('displacedStandAloneMuons'),
-		rsaMuons = cms.InputTag('refittedStandAloneMuons'),
+
+##############################
+#### ANALYSIS COLLECTIONS ####
+##############################
+
+		beamspot       = cms.InputTag('offlineBeamSpot'),
+		vertices       = cms.InputTag('offlinePrimaryVertices'),
+		muons          = cms.InputTag('cleanPatMuons'),
+		gens           = cms.InputTag('prunedGenParticles', '', 'PAT'),
+		GEIP           = cms.InputTag('generator'),
+		dsaMuons       = cms.InputTag('displacedStandAloneMuons'),
+		rsaMuons       = cms.InputTag('refittedStandAloneMuons'),
 	)
 )
