e3fe3c428156285984ef92c1ecde2846e74e9a7a
Slava Valuev <Slava.Valuev@cern.ch>
2018-06-04 15:57:25 +0200

    Save trigger info to tree

--- Tupler/interface/TriggerBranches.h
+++ Tupler/interface/TriggerBranches.h
@@ -26,17 +26,71 @@ class TriggerBranches : public BranchCollection
 		// members
 		static bool alreadyPrinted_;
 
-		bool trig_goodVtx;
+		// Fired path info
+		std::vector<unsigned int> hlt_idx;
+		std::vector<std::string > hlt_path;
+		std::vector<unsigned int> hlt_prescale;
+		std::vector<unsigned int> l1t_prescale;
+
+		// Level-1 muon info
+		std::vector<unsigned int> l1tmu_idx;
+		std::vector<float       > l1tmu_px ;
+		std::vector<float       > l1tmu_py ;
+		std::vector<float       > l1tmu_pz ;
+		std::vector<float       > l1tmu_eta;
+		std::vector<float       > l1tmu_phi;
+
+		// HLT muon info
+		std::vector<unsigned int> hltmu_idx;
+		std::vector<float       > hltmu_px ;
+		std::vector<float       > hltmu_py ;
+		std::vector<float       > hltmu_pz ;
+		std::vector<float       > hltmu_eta;
+		std::vector<float       > hltmu_phi;
 
 		// methods
 		void Declarations()
 		{
-			Declare("trig_goodVtx", trig_goodVtx, "B");
+			Declare("hlt_idx"      , hlt_idx     );
+			Declare("hlt_path"     , hlt_path    );
+       		        Declare("hlt_prescale" , hlt_prescale);
+       		        Declare("l1t_prescale" , l1t_prescale);
+
+			Declare("l1tmu_idx"    , l1tmu_idx   );
+			Declare("l1tmu_px"     , l1tmu_px    );
+			Declare("l1tmu_py"     , l1tmu_py    );
+			Declare("l1tmu_pz"     , l1tmu_pz    );
+			Declare("l1tmu_eta"    , l1tmu_eta   );
+			Declare("l1tmu_phi"    , l1tmu_phi   );
+
+			Declare("hltmu_idx"    , hltmu_idx   );
+			Declare("hltmu_px"     , hltmu_px    );
+			Declare("hltmu_py"     , hltmu_py    );
+			Declare("hltmu_pz"     , hltmu_pz    );
+			Declare("hltmu_eta"    , hltmu_eta   );
+			Declare("hltmu_phi"    , hltmu_phi   );
 		}
 
 		void Reset()
 		{
-			trig_goodVtx = false;
+			hlt_idx     .clear();
+			hlt_path    .clear();
+			hlt_prescale.clear();
+			l1t_prescale.clear();
+
+			l1tmu_idx   .clear();
+			l1tmu_px    .clear();
+			l1tmu_py    .clear();
+			l1tmu_pz    .clear();
+			l1tmu_eta   .clear();
+			l1tmu_phi   .clear();
+
+			hltmu_idx   .clear();
+			hltmu_px    .clear();
+			hltmu_py    .clear();
+			hltmu_pz    .clear();
+			hltmu_eta   .clear();
+			hltmu_phi   .clear();
 		}
 
 		bool Fill(const pat::TriggerEvent& triggerEvent,
