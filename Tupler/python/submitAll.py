357a16bb9dde16453ca4193d0002a7e0513a01c9
rijuvenator <jujudg1031@gmail.com>
2018-06-12 16:32:51 +0200

    Adding first Signal PAT Tuple datasets to the Tupler environment. generateSignalDATFile has been modified significantly; it now additionally gets the PAT Tuple datasets. As the PAT Tuple datasets grow, I foresee this potentially becoming a small issue, but we will deal with that as the datasets get generated, I guess. Worst case scenario they get hard coded. submitAll now reflects the fact that PAT Tuple submissions for signal are distinct from gen only and aod only. SignalMCSamples.dat has been updated for the 2 samples we have. Constants.py now has a new variables, called PATSIGNALPOINTS, which is like the old RECOSIGNALPOINTS in that anything requiring PAT should only run on those signal points.

--- Tupler/python/submitAll.py
+++ Tupler/python/submitAll.py
@@ -1,18 +1,29 @@
 import subprocess as bash
 import DisplacedDimuons.Tupler.Utilities.dataHandler as DH
-from DisplacedDimuons.Common.Constants import SIGNALPOINTS
+from DisplacedDimuons.Common.Constants import SIGNALPOINTS, PATSIGNALPOINTS
+
+# for the time being, we only have a few PAT tuple signal points; these are stored in PATSIGNALPOINTS
+# as of right now, Tuesday June 12 2018 at 15:43 CET, we have 2 signal points for HTo2XTo2Mu2J only
+# When all 66 PAT Tuples are produced, PATSIGNALPOINTS can be removed and everything can be replaced with SIGNALPOINTS
+# Additionally, we have 1 PAT Tuple for data and 1 PAT Tuple for DY100to200; the submissions for exactly those are below
+# In particular, the script skips BG MC for anything except the 1 sample for DY100to200
 
 # change MODE to one of:
 #    --crab  (submit to CRAB)
 #    --batch (submit to LXBATCH)
 #    --test  (run locally for 1000 events)
 #    <empty> (run locally for the entire dataset)
+# you can also append
+#    --verbose  (print out args, cmsRun cfg, and CRAB cfg)
+#    --nosubmit (do not submit this job, whether CRAB, BATCH, or local)
 MODE = '--crab'
 
 # block booleans
 Do_4Mu_GenOnly   = True
-Do_4Mu           = True
+Do_4Mu_AODOnly   = True
+Do_4Mu           = False
 Do_2Mu2J_GenOnly = True
+Do_2Mu2J_AODOnly = True
 Do_2Mu2J         = True
 Do_Background    = True
 Do_Data          = True
@@ -25,13 +36,19 @@ def verbose(message):
 if Do_4Mu_GenOnly:
     for mH, mX, cTau in SIGNALPOINTS:
         verbose('GEN ONLY SIGNAL {} : {} {} {}'.format('HTo2XTo4Mu', mH, mX, cTau))
-        bash.call('python runNTupler.py HTo2XTo4Mu --signalpoint {mH} {mX} {cTau} --genonly {MODE}'.format(**locals()), shell=True)
+        bash.call('python runNTupler.py HTo2XTo4Mu   --signalpoint {mH} {mX} {cTau} --genonly {MODE}'.format(**locals()), shell=True)
 
-# submit all HTo2XTo4Mu signal jobs
-if Do_4Mu:
+# submit all HTo2XTo4Mu signal aod only jobs
+if Do_4Mu_AODOnly:
     for mH, mX, cTau in SIGNALPOINTS:
+        verbose('AOD ONLY SIGNAL {} : {} {} {}'.format('HTo2XTo4Mu', mH, mX, cTau))
+        bash.call('python runNTupler.py HTo2XTo4Mu   --signalpoint {mH} {mX} {cTau} --aodonly {MODE}'.format(**locals()), shell=True)
+
+# submit all HTo2XTo4Mu signal PAT Tuple jobs
+if Do_4Mu:
+    for mH, mX, cTau in PATSIGNALPOINTS:
         verbose('SIGNAL {} : {} {} {}'.format('HTo2XTo4Mu', mH, mX, cTau))
-        bash.call('python runNTupler.py HTo2XTo4Mu --signalpoint {mH} {mX} {cTau} --aodonly {MODE}'.format(**locals()), shell=True)
+        bash.call('python runNTupler.py HTo2XTo4Mu   --signalpoint {mH} {mX} {cTau}           {MODE}'.format(**locals()), shell=True)
 
 # submit all HTo2XTo2Mu2J signal gen only jobs
 if Do_2Mu2J_GenOnly:
@@ -39,11 +56,17 @@ if Do_2Mu2J_GenOnly:
         verbose('GEN ONLY SIGNAL {} : {} {} {}'.format('HTo2XTo2Mu2J', mH, mX, cTau))
         bash.call('python runNTupler.py HTo2XTo2Mu2J --signalpoint {mH} {mX} {cTau} --genonly {MODE}'.format(**locals()), shell=True)
 
-# submit all HTo2XTo2Mu2J signal jobs
-if Do_2Mu2J:
+# submit all HTo2XTo2Mu2J signal aod only jobs
+if Do_2Mu2J_AODOnly:
     for mH, mX, cTau in SIGNALPOINTS:
-        verbose('SIGNAL {} : {} {} {}'.format('HTo2XTo2Mu2J', mH, mX, cTau))
+        verbose('AOD ONLY SIGNAL {} : {} {} {}'.format('HTo2XTo2Mu2J', mH, mX, cTau))
         bash.call('python runNTupler.py HTo2XTo2Mu2J --signalpoint {mH} {mX} {cTau} --aodonly {MODE}'.format(**locals()), shell=True)
+
+# submit all HTo2XTo2Mu2J signal PAT Tuple jobs
+if Do_2Mu2J:
+    for mH, mX, cTau in PATSIGNALPOINTS:
+        verbose('SIGNAL {} : {} {} {}'.format('HTo2XTo2Mu2J', mH, mX, cTau))
+        bash.call('python runNTupler.py HTo2XTo2Mu2J --signalpoint {mH} {mX} {cTau}           {MODE}'.format(**locals()), shell=True)
 
 # submit all background MC jobs
 if Do_Background:
@@ -53,7 +76,9 @@ if Do_Background:
         if data.name == 'DY100to200':
             bash.call('python runNTupler.py {NAME} {MODE}'.format(NAME=data.name, MODE=MODE), shell=True)
         else:
-            bash.call('python runNTupler.py {NAME} --aodonly {MODE}'.format(NAME=data.name, MODE=MODE), shell=True)
+            pass
+            # Do not make nTuples for anything other than the DY100to200 PAT tuple set for now
+            #bash.call('python runNTupler.py {NAME} --aodonly {MODE}'.format(NAME=data.name, MODE=MODE), shell=True)
 
 # submit all data jobs
 if Do_Data:
--- Tupler/scripts/generateSignalDATFile.py
+++ Tupler/scripts/generateSignalDATFile.py
@@ -6,15 +6,23 @@ import DisplacedDimuons.Tupler.Utilities.dataHandler as DH
 ##### and therefore everything else in the ../python directory
 
 # gets HTo2LongLivedTo4mu and HTo2LongLivedTo2mu2jets dataset strings
-DatasetStrings = DH.DASQueryList('dataset=/HTo2LongLivedTo*_MH-*_MFF-*_CTau-*/escalant*/* instance=prod/phys03')
+AODDatasetStrings = DH.DASQueryList('dataset=/HTo2LongLivedTo*_MH-*_MFF-*_CTau-*/escalant*/* instance=prod/phys03')
+
+# gets PAT Tuple datasets -- there may be many of these, and the regexes may have to be tweaked to be unique
+PATDatasetStrings = DH.DASQueryList('dataset=/HTo2LongLivedTo*_MH-*_MFF-*_CTau-*/adasgupt-MC2016_*/* instance=prod/phys03')
 
 # matches dataset string with six groups: final state (4mu or 2mu2jets), mH, mX, cTau (digits 1-4), process string (\S*), weird hash ID (\w*)
-rx = re.compile(r'/HTo2LongLivedTo(4mu|2mu2jets)_MH-(\d{1,4})_MFF-(\d{1,4})_CTau-(\d{1,4})mm\S*pythia8_(\S*)-(\w*)/USER')
+# apply these to AODDatasetStrings
+rxAOD = re.compile(r'/HTo2LongLivedTo(4mu|2mu2jets)_MH-(\d{1,4})_MFF-(\d{1,4})_CTau-(\d{1,4})mm\S*pythia8_(\S*)-(\w*)/USER')
 
-# fill a list with split up metadate that will be sorted
+# matches PAT Tupler dataset string with five groups: final state (4mu or 2mu2jets), mH, mX, cTau (digits 1-4), weird hash ID (\w*)
+# apply these to PATDatasetStrings
+rxPAT = re.compile(r'/HTo2LongLivedTo(4mu|2mu2jets)_MH-(\d{1,4})_MFF-(\d{1,4})_CTau-(\d{1,4})mm\S*pythia8/adasgupt-MC2016_\S*-(\w*)/USER')
+
+# fill a list with split up metadata that will be sorted
 datasets = []
-for ds in DatasetStrings:
-    match = rx.match(ds)
+for ds in AODDatasetStrings:
+    match = rxAOD.match(ds)
     FS      =     match.group(1)
     mH      = int(match.group(2))
     mX      = int(match.group(3))
@@ -22,19 +30,48 @@ for ds in DatasetStrings:
     process =     match.group(5)
     ID      =     match.group(6)
     datasets.append(((mH, mX, cTau, FS), (process, ID, ds)))
+
+for ds in PATDatasetStrings:
+    match = rxPAT.match(ds)
+    FS      =     match.group(1)
+    mH      = int(match.group(2))
+    mX      = int(match.group(3))
+    cTau    = int(match.group(4))
+    ID      =     match.group(5)
+    process = 'PAT'
+    datasets.append(((mH, mX, cTau, FS), (process, ID, ds)))
+
 datasets.sort(key=lambda x: x[0])
 
 # write to file
 f = open('SignalMCSamples.dat', 'w')
 for sp, group in itertools.groupby(datasets, lambda x: x[0]):
     mH, mX, cTau, FS = sp
+    groupList = list(group)
+
+    # set capitalized FS
     if   FS == '4mu'     : FS = '4Mu'
     elif FS == '2mu2jets': FS = '2Mu2J'
-    f    .write('HTo2XTo'+FS                        + '\n') # name
-    f    .write('{} {} {}'.format(mH, mX, cTau)     + '\n') # signal point
-    f    .write('_'                                 + '\n') # PATTuple dataset
-    for data in group:
+
+    # write basic info
+    f        .write('HTo2XTo'+FS                        + '\n') # name
+    f        .write('{} {} {}'.format(mH, mX, cTau)     + '\n') # signal point
+
+    # write PAT dataset, if any
+    for data in groupList:
         process, ID, ds = data[1]
-        f.write('PROCESS {} {}'.format(process, ds) + '\n') # process datasets
-    f    .write('-'                                 + '\n') # end of entry
+        if process == 'PAT':
+            f.write(ds                                  + '\n') # PATTuple dataset
+            break
+    else:
+        f    .write('_'                                 + '\n') # no PATTuple dataset
+
+    # write all other datasets
+    for data in groupList:
+        process, ID, ds = data[1]
+        if process == 'PAT': continue
+        f    .write('PROCESS {} {}'.format(process, ds) + '\n') # process datasets
+
+    # end of entry
+    f        .write('-'                                 + '\n') # end of entry
 f.close()
