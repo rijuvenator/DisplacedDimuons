3121c5c2a31a224e4596fbbb3dadec260002d200
Sebastian Templ <sebastian.templ@cern.ch>
2018-08-22 17:18:40 +0200

    Enable matching of HLT muons, additional exitcode for findDimuon
    - Change matchedMuon function in a way that it can also match HLT muons (the
      previous implementation used the "p4" attribute, which is not available for
      HLT muons). This change also introduces new generic functions for computing
      deltaR and deltaPhi.
    - Add new exitcode ("3") to the findDimuon function, which is returned when the
      passed dimuon list is empty. This scenario resulted in no function return
      value in the previous implementation.

--- Analysis/python/AnalysisTools.py
+++ Analysis/python/AnalysisTools.py
@@ -1,13 +1,18 @@
+import math
+
 # defines a match between a genMuon (Primitives.GenMuon) and a recoMuon (Primitives.RecoMuon)
 # returns list of dictionaries sorted by deltaR of the index of the list, the deltaR gen-reco, and the reco pt
 def matchedMuons(genMuon, recoMuons):
-	matches = []
-	for i,muon in enumerate(recoMuons):
-		deltaR = muon.p4.DeltaR(genMuon.p4)
-		#if deltaR < min(0.3,genMuon.deltaR) and Selections.CUTS['pT'].apply(muon) and muon.charge == genMuon.charge:
-		if deltaR < 0.3:
-			matches.append({'idx':i, 'deltaR':deltaR, 'pt':muon.pt})
-	return sorted(matches, key=lambda dic:dic['deltaR'])
+    matches = []
+    if len(recoMuons) == 0:
+        return matches
+
+    for i,muon in enumerate(recoMuons):
+        dR = deltaR(genMuon, muon)
+        #if dR < min(0.3,genMuon.dR) and Selections.CUTS['pT'].apply(muon) and muon.charge == genMuon.charge:
+        if dR < 0.3:
+            matches.append({'idx':i, 'deltaR':dR, 'pt':muon.pt})
+    return sorted(matches, key=lambda dic:dic['deltaR'])
 
 # defines a match between a genMuonPair (Primitives.GenMuon) and a dimuon (Primitives.Dimuon)
 # returns list of dictionaries sorted by deltaR of the index of the list, the deltaR gen-reco, and the reco pt
@@ -26,9 +31,12 @@ def matchedDimuons(genMuonPair, dimuons):
 # exitcode 0 means dimuon was found (redundant with dimuon is not None)
 # exitcode 1 means both gen muons did not match
 # exitcode 2 means both gen muons matched, but a suitable dimuon was not found
+# exitcode 3 means the dimuon list was empty and nothing was matched
 # muonMatches are the indices of the matching reco muons
 def findDimuon(genMuonPair, recoMuons, dimuons):
     muonMatches = [None, None]
+    if len(dimuons) == 0: return None, 3, muonMatches
+
     for i, genMuon in enumerate(genMuonPair):
         matches = matchedMuons(genMuon, recoMuons)
         if len(matches) > 0:
@@ -42,3 +50,17 @@ def findDimuon(genMuonPair, recoMuons, dimuons):
             return None, 2, muonMatches
     else:
         return None, 1, muonMatches
+
+
+def deltaR(p1, p2):
+    dphi = deltaPhi(p1, p2)
+    deta = p1.eta - p2.eta
+    return math.sqrt(dphi*dphi + deta*deta)
+
+
+def deltaPhi(p1, p2):
+    dphi = abs(p1.phi - p2.phi)
+    if dphi > math.pi:
+        dphi = 2*math.pi - dphi
+    return dphi
+
