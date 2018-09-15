cb81e0956dc21bc476164162937d96a987a9d1c8
William <wnash@bu.edu>
2018-09-15 15:51:39 -0500

    Updating d0 plots

--- Analysis/analyzers/analyzeD0.py
+++ Analysis/analyzers/analyzeD0.py
@@ -1,66 +1,75 @@
 #!/usr/bin/env python
 import ROOT as R
+import DisplacedDimuons.Analysis.Analyzer as Analyzer
 import DisplacedDimuons.Analysis.Primitives as Primitives
 
-TESTFILE = '../test/test.root'
-OUTFILE = 'd0-plots.root'
+#
+# voms-proxy-init --voms cms
+#python analyzeD0.py --signalpoint 125 20 1300
+#
 
-f = R.TFile.Open(TESTFILE)
-t = f.Get('SimpleNTupler/DDTree')
-f1 = R.TFile(OUTFILE, "RECREATE")
-f.cd()
 
-# test tree
-try:
-    t.GetEntries()
-    print('Successfully got tree...')
-except:
-    print('Failed to get tree; exiting')
-    exit()
+#define all combinations of d0s
+d0Types = []
+for ex in Primitives.ImpactParameter.extrapolationDictionary:
+    for vert in Primitives.ImpactParameter.vertexDictionary:
+        d0_str = vert + "-" 
+        if ex == None:
+            d0_str += "FULL"
+        else:
+            d0_str += ex
+        d0Types.append([vert, ex, d0_str])
+
+print d0Types
+
+
+
+def declareHistograms(self, Params=None):
+    #iterate through all combinations of d0 values
+    for i, d0_1_t in enumerate(d0Types):
+        for j, d0_2_t in enumerate(d0Types):
+            if j <= i: continue
+            
+            d0_1_str = d0_1_t[2]
+            d0_2_str = d0_2_t[2]
+            self.HistInit('%s-%s'%(d0_1_str, d0_2_str), "%s - %s; d_{0}[cm]; Counts"%(d0_1_str, d0_2_str), 100, 1,-1)
+            
     
     
-
-
-d0Types = [['PV-FullFit','PV',None], ['PV-LineFit', 'PV','LIN'], ['BS-FullFit', 'BS', None], ['BS-LineFit', 'BS', 'LIN']]
-
-#histograms for comparing d0 quantities
-hists = {}
-counter = 0
-for i, d0_t1 in enumerate(d0Types):
-    for j, d0_t2 in enumerate(d0Types):
-        if j <= i: continue
-        
-        hists[counter] = R.TH1F("%s_%s"%(d0_t1[0], d0_t2[0]), "%s - %s; d0[units?], Muons"%(d0_t1[0],d0_t2[0]), 100, -1,1)
-        counter += 1
-        
-
-for e, event in enumerate(t):
-    E = Primitives.ETree(t)
-        
-    Gens     = E.getPrimitives('GEN')
-    DSAMuons = E.getPrimitives('DSAMUON')
-          
-    for mu in DSAMuons:
-        counter = 0
-        for i, d0_t1 in enumerate(d0Types):
-            for j, d0_t2 in enumerate(d0Types):
+def analyze(self, E, PARAMS=None):
+    
+    Muons = E.getPrimitives('DSAMUON')
+    
+    #selections
+    if self.TRIGGER:
+        if not Selections.passTrigger(E): return
+    
+    for mu in Muons:    
+        for i, d0_1_t in enumerate(d0Types):
+            for j, d0_2_t in enumerate(d0Types):
                 if j <= i: continue
-                d0_1 = mu.d0(d0_t1[1], d0_t1[2])  
-                d0_2 = mu.d0(d0_t2[1],d0_t2[2])
-                
-                hists[counter].Fill(d0_1-d0_2)
+                d0_1 = mu.d0(d0_1_t[0], d0_1_t[1])
+                d0_2 = mu.d0(d0_2_t[0], d0_2_t[1])
+                if (abs(d0_1 -d0_2) > 0.1):
+                    event = E.getPrimitives('EVENT')
+                    print event
+                    print mu
+            
+                d0_1_str = d0_1_t[2]
+                d0_2_str = d0_2_t[2]
+                self.HISTS['%s-%s'%(d0_1_str,d0_2_str)].Fill(d0_1 - d0_2)
+            
 
-                counter +=1
-       # hists.append(R.TH1F("%s_%s"%(d0_1[0], d0_2[0]), "%s - %s; d0[units?], Muons"%(d0_1[0],d0_2[0]), 100, -5,5))
-        
 
-print("Writing Histograms")
 
-f1.cd()
-for hist in hists:
-    hists[hist].SetDirectory(0)
-    hists[hist].Write()
-    
-f1.Close()
-
-   
+#### RUN ANALYSIS ####
+if __name__ == '__main__':
+    ARGS = Analyzer.PARSER.parse_args()
+    Analyzer.setSample(ARGS)
+    for METHOD in ('declareHistograms', 'analyze'):
+        setattr(Analyzer.Analyzer, METHOD, locals()[METHOD])
+    analyzer = Analyzer.Analyzer(
+        ARGS        = ARGS,
+        BRANCHKEYS  = ('EVENT','GEN', 'DSAMUON'),
+    )
+    analyzer.writeHistograms('roots/d0ComparisonPlots{}_{{}}.root'.format('_Trig' if ARGS.TRIGGER else ''))
