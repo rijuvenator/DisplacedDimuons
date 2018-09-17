edc543fb54709328bed788b873a7eac3bd959cf2
William Nash <wnash@ucla.edu>
2018-09-17 13:48:08 -0700

    Added dz

--- Analysis/analyzers/analyzeD0.py
+++ Analysis/analyzers/analyzeD0.py
@@ -37,12 +37,14 @@ def declareHistograms(self, Params=None):
     #iterate through all combinations of d0 values
     for i, d0_1_t in enumerate(d0Types):
         d0_1_str = d0_1_t[2]
-        self.HistInit('Gen-%s'%d0_1_str, "Gen - %s; d_{0}[cm]; Counts"%d0_1_str,100 ,-4,4)
+        self.HistInit('d0 Gen-%s'%d0_1_str, "Gen - %s; d_{0}[cm]; Counts"%d0_1_str,100 ,-4,4)
+        self.HistInit('dz Gen-%s'%d0_1_str, "Gen - %s; d_{z}[cm]; Counts"%d0_1_str,100 ,-4,4)
         for j, d0_2_t in enumerate(d0Types):
             if j <= i: continue
             
             d0_2_str = d0_2_t[2]
-            self.HistInit('%s-%s'%(d0_1_str, d0_2_str), "%s - %s; d_{0}[cm]; Counts"%(d0_1_str, d0_2_str), 100, -0.015,0.015)
+            self.HistInit('d0 %s-%s'%(d0_1_str, d0_2_str), "%s - %s; d_{0}[cm]; Counts"%(d0_1_str, d0_2_str), 100, -0.015,0.015)
+            self.HistInit('dz %s-%s'%(d0_1_str, d0_2_str), "%s - %s; d_{z}[cm]; Counts"%(d0_1_str, d0_2_str), 100, -0.015,0.015)
             
     
 def analyze(self, E, PARAMS=None):
@@ -65,18 +67,22 @@ def analyze(self, E, PARAMS=None):
             d0_1_str = d0_1_t[2]
             closestGen = findClosestGenMuon(mu, genMuons)
             d0_1 = mu.d0(d0_1_t[0], d0_1_t[1])
+            dz_1 = mu.dz(d0_1_t[0], d0_1_t[1])
             if closestGen is not None:
-                self.HISTS['Gen-%s'%d0_1_str].Fill(closestGen.d0() - d0_1)
+                self.HISTS['d0 Gen-%s'%d0_1_str].Fill(closestGen.d0() - d0_1)
+                self.HISTS['dz Gen-%s'%d0_1_str].Fill(closestGen.dz() - dz_1)
             for j, d0_2_t in enumerate(d0Types):
                 if j <= i: continue
                 d0_2 = mu.d0(d0_2_t[0], d0_2_t[1])
+                dz_2 = mu.dz(d0_2_t[0], d0_2_t[1])
                 if (abs(d0_1 -d0_2) > 0.1):
                     event = E.getPrimitives('EVENT')
                     print event
                     print mu
             
                 d0_2_str = d0_2_t[2]
-                self.HISTS['%s-%s'%(d0_1_str,d0_2_str)].Fill(d0_1 - d0_2)
+                self.HISTS['d0 %s-%s'%(d0_1_str,d0_2_str)].Fill(d0_1 - d0_2)
+                self.HISTS['dz %s-%s'%(d0_1_str,d0_2_str)].Fill(dz_1 - dz_2)
 
 
 #### RUN ANALYSIS ####
