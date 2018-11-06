c126a677e5f2a77545c5aa28822c10699b1b7085
Sebastian Templ <sebastian.templ@cern.ch>
2018-11-06 14:53:37 +0100

    Add class for color palette definitions
    Simple "color state machine" that defines custom palettes with ROOT colors,
    stores a color state (that can be retreived at any time), and allows easy
    cycling through these colors (while automatically returning to the first color
    when reaching the end of the palette).
    This is particularly convenient when looping over many histograms/graphs while
    requiring different and distinctive colors for each of them.
    
    Example usage:
    
    ```
    palette = Plotter.ColorPalette(palette='defaultPalette')
    
    color1 = palette.getNextColor()
    color2 = palette.getNextColor()
    palette.resetColor()
    color3 = palette.getColor()
    ```

--- Analysis/python/Plotter.py
+++ Analysis/python/Plotter.py
@@ -605,3 +605,48 @@ class Canvas(R.TCanvas):
         #R.SetOwnership(self, False)
         self.save(filename)
         self.deleteCanvas()
+
+# Simple class for color palette definitions ("color state machine"):
+# palette: Class objects can be initialized with the 'palette' argument being
+# the name of a pre-defined palette or a user-defined list of colors (i.e.,
+# integers in the case of ROOT).
+# colorState: position of the color to start with (defaults to the beginning of
+# the color palette list, i.e., zero)
+# getColor(): returns the current color state
+# getNextColor(): returns the current color state and sets the color state to
+# the next color in the palette (modulo the total number of colors)
+# resetColor(): reverts the color state to a given color in the palette
+# (default: reset to the beginning of the palette)
+class ColorPalette:
+    __palettes = {
+        'defaultPalette': [600,632,417,867,921,811,616,397,433,874,797,833]
+    }
+
+    def __init__(self, palette='defaultPalette', colorState=0):
+        self.colorState = colorState
+        if isinstance(palette, str) and palette in self.__palettes.keys():
+            self.palette = self.__palettes[palette]
+        elif isinstance(palette, list):
+            self.palette = palette
+        else:
+            print('Warning: Invalid palette. Using default palette.')
+            self.palette = self.__palettes['defaultPalette']
+
+    def getColor(self):
+        return self.colorState
+
+    def getNextColor(self):
+        color = self.palette[self.colorState]
+        if self.colorState < len(self.palette)-1:
+            self.colorState += 1
+        else:
+            self.colorState = 0
+        return color
+
+    def resetColor(self, colorState=0):
+        if colorState < len(self.palette):
+            self.colorState = colorState
+        else:
+            print('Warning: Color index outside of the palette range.')
+            self.colorState = 0
+
