5fece60c38b4c5d717bc3c0775880af12e88f3bf
rijuvenator <jujudg1031@gmail.com>
2019-07-03 10:04:56 +0200

    adding KS probability and extra ratio and dist plots to BGest

--- Analysis/bgest/makeValidationPlots.py
+++ Analysis/bgest/makeValidationPlots.py
@@ -10,23 +10,21 @@ h = {k:D[k]['data'] for k in D}
 
 c = {k:h[k].GetCumulative(False) for k in h}
 
-z = c['LxySig_IDPHI'].Clone()
-z.Divide(c['LxySig_DPHI'])
+cumRatio = c['LxySig_IDPHI'].Clone()
+cumRatio.Divide(c['LxySig_DPHI'])
 
-pDist = {k:Plotter.Plot(h[k], '#Delta#Phi {} #pi/2'.format('<' if '_DPHI' in k else '>'), 'lp', 'hist') for k in h}
-pCum  = {k:Plotter.Plot(c[k], '#Delta#Phi {} #pi/2'.format('<' if '_DPHI' in k else '>'), 'lp', 'hist') for k in c}
-pz    = Plotter.Plot(z, '', '', 'hist')
+for k in h:
+    h[k].Rebin(5)
+
+distRatio = h['LxySig_IDPHI'].Clone()
+distRatio.Divide(h['LxySig_DPHI'])
+
+pDist      = {k:Plotter.Plot(h[k], '#Delta#Phi {} #pi/2'.format('<' if '_DPHI' in k else '>'), 'lp', 'hist e') for k in h}
+pCum       = {k:Plotter.Plot(c[k], '#Delta#Phi {} #pi/2'.format('<' if '_DPHI' in k else '>'), 'lp', 'hist') for k in c}
+pCumRatio  = Plotter.Plot(cumRatio, '', '', 'hist')
+pDistRatio = Plotter.Plot(distRatio, '', '', 'hist')
 
 #### Plot 1
-canvas = Plotter.Canvas()
-canvas.addMainPlot(pDist['LxySig_IDPHI'])
-canvas.addMainPlot(pDist['LxySig_DPHI'])
-pDist['LxySig_IDPHI'].setColor(R.kRed )
-pDist['LxySig_DPHI' ].setColor(R.kBlue)
-canvas.makeLegend(lWidth=.2)
-canvas.cleanup('dist.pdf')
-
-#### Plot 2
 canvas = Plotter.Canvas()
 canvas.addMainPlot(pCum['LxySig_IDPHI'])
 canvas.addMainPlot(pCum['LxySig_DPHI'])
@@ -35,9 +33,28 @@ pCum ['LxySig_DPHI' ].setColor(R.kBlue)
 canvas.makeLegend(lWidth=.2)
 canvas.cleanup('cum.pdf')
 
+#### Plot 2
+canvas = Plotter.Canvas()
+canvas.addMainPlot(pCumRatio)
+pCumRatio.setColor(R.kBlue)
+pCumRatio.setTitles(Y='Ratio of CR/SR')
+canvas.cleanup('ratio.pdf')
+
 #### Plot 3
 canvas = Plotter.Canvas()
-canvas.addMainPlot(pz)
-pz.setColor(R.kBlue)
-pz.setTitles(Y='Ratio of CR/SR')
-canvas.cleanup('ratio.pdf')
+canvas.addMainPlot(pDist['LxySig_IDPHI'])
+canvas.addMainPlot(pDist['LxySig_DPHI'])
+pDist['LxySig_IDPHI'].setColor(R.kRed )
+pDist['LxySig_DPHI' ].setColor(R.kBlue)
+canvas.makeLegend(lWidth=.2)
+canvas.cleanup('dist.pdf')
+
+#### Plot 4
+canvas = Plotter.Canvas()
+canvas.addMainPlot(pDistRatio)
+pDistRatio.setColor(R.kBlue)
+pDistRatio.setTitles(Y='Ratio of CR/SR')
+canvas.cleanup('ratioDist.pdf')
+
+#### KS Probability
+print h['LxySig_IDPHI'].KolmogorovTest(h['LxySig_DPHI'])
